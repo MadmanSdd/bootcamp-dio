b2d39ae9a9203501f9026adbc41759c6b5c329ca
Madman <44852253+MadmanSdd@users.noreply.github.com>
2022-01-23 12:08:02 -0300

    sobrecargas

---
+++ metodo/src/sobrecargas/Main.java
@@ -0,0 +1,12 @@
+package sobrecargas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sobrecargas.soma(1, 3);
+		Sobrecargas.soma(3, 22, 1);
+		Sobrecargas.soma(1, 2, 3, 4);
+	}
+
+}
---
+++ metodo/src/sobrecargas/Sobrecargas.java
@@ -0,0 +1,17 @@
+package sobrecargas;
+
+public class Sobrecargas {
+	public static void soma(int valor,int valor2) {
+		System.out.println("Valor "+(valor+valor2));
+	}
+	
+	public static void soma(int valor,int valor2,int valor3) {
+		System.out.println("Valor "+(valor+valor2+valor3));
+	}
+	
+	public static void soma(int valor,int valor2,int valor3,int valor4) {
+		System.out.println("Valor "+(valor+valor2+valor3));
+	}
+
+
+}
